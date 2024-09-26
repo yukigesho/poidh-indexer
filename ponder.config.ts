1cdcbb2847a1641f59f8b98faf2bfe245f65c24d
yukigesho <yukigesho@proton.me>
2024-09-26 16:06:51 -0700

    Edit `ponder.config.ts' and add `abis`

--- ponder.config.ts
+++ ponder.config.ts
@@ -1,21 +1,62 @@
 import { createConfig } from "@ponder/core";
 import { http } from "viem";
-
-import { ExampleContractAbi } from "./abis/ExampleContractAbi";
+import PoidhV2ABI from "./abis/PoidhV2Abi";
+import PoidhV2NFTABI from "./abis/PoidhV2NFTAbi";
 
 export default createConfig({
   networks: {
-    mainnet: {
-      chainId: 1,
-      transport: http(process.env.PONDER_RPC_URL_1),
+    base: {
+      chainId: 8453,
+      transport: http(process.env.BASE_RPC_URL),
+    },
+    degen: {
+      chainId: 666666666,
+      transport: http(process.env.DEGEN_RPC_URL),
+    },
+    arbitrum: {
+      chainId: 42161,
+      transport: http(process.env.ARBITRUM_RPC_URL),
     },
   },
   contracts: {
-    ExampleContract: {
-      network: "mainnet",
-      abi: ExampleContractAbi,
-      address: "0x0000000000000000000000000000000000000000",
-      startBlock: 1234567,
+    // base
+    PoidhBaseContract: {
+      network: "base",
+      abi: PoidhV2ABI,
+      address: "0xb502c5856F7244DccDd0264A541Cc25675353D39",
+      startBlock: 14542727,
+    },
+    PoidhNFTBaseContract: {
+      network: "base",
+      abi: PoidhV2NFTABI,
+      address: "0xDdfb1A53E7b73Dba09f79FCA24765C593D447a80",
+      startBlock: 14542570,
+    },
+    // degen
+    PoidhDegenContract: {
+      network: "degen",
+      abi: PoidhV2ABI,
+      address: "0x2445BfFc6aB9EEc6C562f8D7EE325CddF1780814",
+      startBlock: 6991084,
+    },
+    PoidhNFTDegenContract: {
+      network: "degen",
+      abi: PoidhV2NFTABI,
+      address: "0xDdfb1A53E7b73Dba09f79FCA24765C593D447a80",
+      startBlock: 4857281,
+    },
+    // arbitrum
+    PoidhArbitrumContract: {
+      network: "arbitrum",
+      abi: PoidhV2ABI,
+      address: "0x0Aa50ce0d724cc28f8F7aF4630c32377B4d5c27d",
+      startBlock: 211898523,
+    },
+    PoidhNFTArbitrumContract: {
+      network: "arbitrum",
+      abi: PoidhV2NFTABI,
+      address: "0xDdfb1A53E7b73Dba09f79FCA24765C593D447a80",
+      startBlock: 211898311,
     },
   },
 });
