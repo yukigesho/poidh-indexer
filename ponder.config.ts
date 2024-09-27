9e56c5133e504ceef65a5d50270e22dc81d090c1
yukigesho <yukigesho@proton.me>
2024-09-26 18:24:32 -0700

    Refactor `poder.config`

--- ponder.config.ts
+++ ponder.config.ts
@@ -19,44 +19,39 @@ export default createConfig({
     },
   },
   contracts: {
-    // base
-    PoidhBaseContract: {
-      network: "base",
+    PoidhContract: {
       abi: PoidhV2ABI,
-      address: "0xb502c5856F7244DccDd0264A541Cc25675353D39",
-      startBlock: 14542727,
+      network: {
+        base: {
+          address: "0xb502c5856F7244DccDd0264A541Cc25675353D39",
+          startBlock: 14542727,
+        },
+        degen: {
+          address: "0x2445BfFc6aB9EEc6C562f8D7EE325CddF1780814",
+          startBlock: 6991084,
+        },
+        arbitrum: {
+          address: "0x0Aa50ce0d724cc28f8F7aF4630c32377B4d5c27d",
+          startBlock: 211898523,
+        },
+      },
     },
-    PoidhNFTBaseContract: {
-      network: "base",
+    PoidhNFTContract: {
       abi: PoidhV2NFTABI,
-      address: "0xDdfb1A53E7b73Dba09f79FCA24765C593D447a80",
-      startBlock: 14542570,
-    },
-    // degen
-    PoidhDegenContract: {
-      network: "degen",
-      abi: PoidhV2ABI,
-      address: "0x2445BfFc6aB9EEc6C562f8D7EE325CddF1780814",
-      startBlock: 6991084,
-    },
-    PoidhNFTDegenContract: {
-      network: "degen",
-      abi: PoidhV2NFTABI,
-      address: "0xDdfb1A53E7b73Dba09f79FCA24765C593D447a80",
-      startBlock: 4857281,
-    },
-    // arbitrum
-    PoidhArbitrumContract: {
-      network: "arbitrum",
-      abi: PoidhV2ABI,
-      address: "0x0Aa50ce0d724cc28f8F7aF4630c32377B4d5c27d",
-      startBlock: 211898523,
-    },
-    PoidhNFTArbitrumContract: {
-      network: "arbitrum",
-      abi: PoidhV2NFTABI,
-      address: "0xDdfb1A53E7b73Dba09f79FCA24765C593D447a80",
-      startBlock: 211898311,
+      network: {
+        base: {
+          address: "0xDdfb1A53E7b73Dba09f79FCA24765C593D447a80",
+          startBlock: 14542570,
+        },
+        degen: {
+          address: "0xDdfb1A53E7b73Dba09f79FCA24765C593D447a80",
+          startBlock: 4857281,
+        },
+        arbitrum: {
+          address: "0xDdfb1A53E7b73Dba09f79FCA24765C593D447a80",
+          startBlock: 211898311,
+        },
+      },
     },
   },
 });
