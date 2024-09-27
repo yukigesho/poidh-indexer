4225c9c65c5557ed024d30755bf15643af33b4e3
yukigesho <yukigesho@proton.me>
2024-09-26 17:29:15 -0700

    Add `schema`

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,8 +1,42 @@
 import { createSchema } from "@ponder/core";
 
 export default createSchema((p) => ({
-  Example: p.createTable({
-    id: p.string(),
-    name: p.string().optional(),
+  Bounty: p.createTable({
+    id: p.int(),
+    issuerId: p.int().references("User.id"),
+    chainId: p.int(),
+    title: p.string(),
+    description: p.string(),
+    amount: p.string(),
+    createdAt: p.int(),
+    isMultiplayes: p.boolean(),
+    inProgress: p.boolean(),
+    isBanned: p.boolean(),
+
+    claims: p.many("Claim.bountyId"),
+    issuer: p.one("issuerId"),
+  }),
+
+  User: p.createTable({
+    id: p.int(),
+    address: p.string(),
+    ens: p.string().optional(),
+    degenName: p.string().optional(),
+
+    bounties: p.many("Bounty.issuerId"),
+    claims: p.many("Claim.issuerId"),
+  }),
+
+  Claim: p.createTable({
+    id: p.int(),
+    bountyId: p.int().references("Bounty.id"),
+    issuerId: p.int().references("User.id"),
+    title: p.string(),
+    description: p.string(),
+    createdAt: p.int(),
+    isBanned: p.boolean(),
+
+    issuer: p.one("issuerId"),
+    bounty: p.one("bountyId"),
   }),
 }));
