36c406fae79c99dcdb124723c409779ac56d5999
whtsupbab3 <105609896+whtsupbab3@users.noreply.github.com>
2025-11-26 09:01:12 -0500

    Notify when bounty reaches price of 100$
    * Add notifications on claim accepted
    
    * Add notification on ClaimCreated
    
    * Add notification on bountyjoined >= 100$
    
    * Add notification on ClaimSubmittedForVote
    
    ---------
    
    Co-authored-by: wtsupbab3 <wtsupbab3@gmail.com>

--- src/Poidh.ts
+++ src/Poidh.ts
@@ -144,7 +144,7 @@ ponder.on("PoidhContract:BountyJoined", async ({ event, context }) => {
     args: [bountyId],
   });
 
-  await database
+  const updatedBounty = await database
     .update(bounties, {
       id: Number(bountyId),
       chainId: context.chain.id,
@@ -178,6 +178,25 @@ ponder.on("PoidhContract:BountyJoined", async ({ event, context }) => {
     chainId: context.chain.id,
     timestamp,
   });
+
+  const joinedAmountUsd =
+    Number(formatEther(BigInt(amount))) *
+    (context.chain.id === 666666666
+      ? Number(price!.degen_usd)
+      : Number(price!.eth_usd));
+  if (
+    isLive(event.block.timestamp) &&
+    updatedBounty.amountSort - joinedAmountUsd < 100
+  ) {
+    const creatorName = await getDisplayName(updatedBounty.issuer);
+    await sendNotification({
+      title: `💰 NEW $${updatedBounty.amountSort.toFixed(0)} BOUNTY 💰`,
+      messageBody: `${updatedBounty.title}${
+        creatorName ? ` from ${creatorName}` : ""
+      }`,
+      targetUrl: `${POIDH_BASE_URL}/${context.chain.name}/bounty/${updatedBounty.id}`,
+    });
+  }
 });
 
 ponder.on(
@@ -454,7 +473,7 @@ ponder.on("PoidhContract:ClaimSubmittedForVote", async ({ event, context }) => {
     blockNumber: event.block.number,
   });
 
-  await database
+  const updatedBounty = await database
     .update(bounties, {
       id: Number(bountyId),
       chainId: context.chain.id,
@@ -473,6 +492,29 @@ ponder.on("PoidhContract:ClaimSubmittedForVote", async ({ event, context }) => {
     chainId: context.chain.id,
     timestamp,
   });
+
+  if (isLive(timestamp)) {
+    const submittedClaim = await database.sql.query.claims.findFirst({
+      where: (table, { and, eq }) =>
+        and(eq(table.id, Number(claimId)), eq(table.chainId, context.chain.id)),
+    });
+    if (!submittedClaim) return;
+
+    const targetFarcasterUser = await getFarcasterUsers([
+      submittedClaim.issuer.toLowerCase(),
+    ]);
+    const targetFId =
+      targetFarcasterUser[submittedClaim.issuer.toLowerCase()]?.[0]?.fid;
+
+    if (targetFId) {
+      await sendNotification({
+        title: `your claim is nominated 🗳️`,
+        messageBody: `your claim is up for vote for ${updatedBounty.title} - contributors will now vote to confirm`,
+        targetUrl: `${POIDH_BASE_URL}/${context.chain.name}/bounty/${updatedBounty.id}`,
+        targetFIds: [targetFId],
+      });
+    }
+  }
 });
 
 ponder.on("PoidhContract:VoteClaim", async ({ event, context }) => {
